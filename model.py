e380a884c1f5289f4703622cfcd3fa0f033cda0f
R <ryansereno@protonmail.com>
2023-01-16 18:25:39 -0500

    added show image and grid functions

--- model.py
+++ model.py
@@ -1,4 +1,32 @@
 from diffusers import DDPMPipeline
+from diffusers import StableDiffusionPipeline
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from PIL import Image
+
+
+def show_images(x):
+    """Given a batch of images x, make a grid and convert to PIL"""
+    x = x * 0.5 + 0.5  # Map from (-1, 1) back to (0, 1)
+    grid = torchvision.utils.make_grid(x)
+    grid_im = grid.detach().cpu().permute(1, 2, 0).clip(0, 1) * 255
+    grid_im = Image.fromarray(np.array(grid_im).astype(np.uint8))
+    return grid_im
+
+
+def make_grid(images, size=64):
+    """Given a list of PIL images, stack them together into a line for easy viewing"""
+    output_im = Image.new("RGB", (size * len(images), size))
+    for i, im in enumerate(images):
+        output_im.paste(im.resize((size, size)), (i * size, 0))
+    return output_im
+
+
+# Mac users may need device = 'mps' (untested)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the butterfly pipeline
 butterfly_pipeline = DDPMPipeline.from_pretrained(
